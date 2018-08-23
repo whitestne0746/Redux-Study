f2477c113c5151beb04f326bef5922f40c6a1a6a
yudai.shiraishi <shiraishi.re1554@gmail.com>
2018-08-23 22:11:50 +0900

    fix

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function TodoApp({ task, tasks, inputTask, addTask }) {
+export default function TodoApp({task, tasks, inputTask, addTask}) {
   return (
     <div>
       <input type="text" onChange={(e) => inputTask(e.target.value)} />
       <input type="button" value="add" onClick={() => addTask(task)} />
       <ul>
         {
-          tasks.map(function (item, i) {
+          tasks.map((item, i) => {
             return (
               <li key={i}>{item}</li>
             );
